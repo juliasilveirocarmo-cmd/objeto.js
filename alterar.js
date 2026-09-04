12c88eb80434ea3ac0861e84fc587133d3f4a3ad
juliasilveirocarmo-cmd <julia.silveiro.carmo@escola.pr.gov.br>
2026-09-04 14:30:05 +0000

    jkjgjfkjjfkgf

--- alterar.js
+++ alterar.js
@@ -4,3 +4,19 @@ const pessoa = {
 }
 
 console.log(pessoa.nome);
+
+console.log(pessoa.telefone);
+
+pessoa.telefone = "123456789";
+
+console.log(pessoa.telefone);
+
+pessoa.nome = "Luma Silva";
+
+console.log(pessoa.nome);
+
+const novaPessoa = {
+nome: "Pedro",
+};
+
+pessoa = novaPessoa;
