56f7bcc6fdebcdda974fef6223ad52654a0abb50
juliasilveirocarmo-cmd <julia.silveiro.carmo@escola.pr.gov.br>
2026-09-04 14:56:10 +0000

    julia

--- tipo.js
+++ tipo.js
@@ -2,5 +2,7 @@ const cliente = {
     nome: "Joao",
     idade: 24,
     email: "joao@firma.com",
-    telefone: "1155555550",
-}
+        telefone: ["1155555550", "144444440"],
+};
+
+console.log(cliente);
